96f858d326658342d896a122b540835d9df33d60
Benjamin Wölkchen <bwolkchen@gmail.com>
2019-05-14 16:10:50 +0200

    Commit

---
+++ Bibel4000site/app/src/main/java/com/wp/bibel4000site/Bibel4000site.java
@@ -0,0 +1,6 @@
+package com.wp.bibel4000site;
+
+import android.app.Application;
+
+public class Bibel4000site extends Application {
+}
---
+++ Bibel4000site/app/src/main/java/com/wp/bibel4000site/MainActivity.java
@@ -0,0 +1,28 @@
+package com.wp.bibel4000site;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webb =(WebView)findViewById(R.id.web1);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("https://bibel4000site.wordpress.com");
+
+
+        WebSettings webSettings = webb.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webb.setWebChromeClient(new WebChromeClient());
+
+
+    }
+}
